3cddd45e955d92a581fb9640de115718cd399d4f
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-20 10:50:24 +0200

    Subscribe to a resource
    To test:
    @post = Post.find(1)
    @comment = @post.comments.build(commenter: 'Zoey', body: 'I am a dog')
    @comment.save
    PostsChannel.broadcast_to(@post, @comment)

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,7 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    def connect
+      puts '**** Connection#connect ****'
+    end
   end
 end
---
+++ app/channels/posts_channel.rb
@@ -0,0 +1,13 @@
+class PostsChannel < ApplicationCable::Channel
+  def subscribed
+    puts '**** PostsChannel#subscribed ****'
+
+    # stream_from "some_channel"
+    post = Post.find(params[:id])
+    stream_for post
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
