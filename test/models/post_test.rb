a568101115dee6e832a3842ba981abcbcf3ede6e
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-21 10:15:16 +0200

    Write first test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save post without title' do
+    post = Post.new
+    assert_not post.save, 'Save the article without a title'
+  end
+
+  test 'should report error' do
+    # some_undefined_variable
+    # assert true
+
+    assert_raises(NameError) do
+      some_undefined_variable
+    end
+  end
 end
