f63745b65b55995df725e819ebefedb5b0a1e324
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-22 10:32:40 +0200

    Add system test

---
+++ test/system/posts_test.rb
@@ -0,0 +1,13 @@
+require "application_system_test_case"
+
+class PostsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit posts_url
+  #
+  #   assert_selector "h1", text: "Posts"
+  # end
+  test 'viewing the index' do
+    visit posts_path
+    assert_selector 'h1', text: 'Posts'
+  end
+end
