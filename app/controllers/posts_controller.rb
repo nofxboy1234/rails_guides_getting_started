7ab55f3bbd132901b52c0b0821f8ed67adf8f4ce
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-22 11:10:53 +0200

    Add "can create a post" integration test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,27 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
--- test/integration/blog_flow_test.rb
+++ test/integration/blog_flow_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class BlogFlowTest < ActionDispatch::IntegrationTest
   # test "the truth" do
@@ -8,5 +8,16 @@ class BlogFlowTest < ActionDispatch::IntegrationTest
   test 'can see the posts page' do
     get '/'
     assert_select 'h1', 'Posts'
+  end
+
+  test 'can create a post' do
+    get '/posts/new'
+    assert_response :success
+
+    post '/posts', params: { post: { title: 'can create', body: 'post successfully' } }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select 'p', "Body: post successfully"
   end
 end
